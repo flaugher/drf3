b8ffd36523dc9e6c12ff8c6c8264df5fa1d4f751
Robert Flaugher <rlfprog@gmail.com>
2018-04-18 10:46:16 -0700

    Add DRF settings, urls code, update README
    Update README.
    
    Add DRF settings.
    
    Add DRF code to main urls.

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ These are examples from [Django REST framework 3](http://www.django-rest-framewo
 
 Use your "drf3" virtual environment, not django184.
 
+#### Add Users and Groups
+
+    # Create superuser
+    python manage.py createsuperuser
+
+Log in to localhost:8000/admin and add them from there.
+
--- conf/settings.py
+++ conf/settings.py
@@ -111,3 +111,12 @@ STATIC_URL = '/static/'
 
 # For Django Debug Toolbar
 INTERNAL_IPS = ('127.0.0.1', )
+
+# For DRF
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
